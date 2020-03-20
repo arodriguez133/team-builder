7a307e6e7d6763abb9c0ea6954743b89d3a80d09
Adan Rodriguez <arodriguez_11@live.com>
2020-03-19 19:18:48 -0500

    created file for form and added new Card structure for form

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+
+//Card Structure
+const Form = props => {
+    //Card structure
+    const [card, setCard] = useState ({
+        name: "",
+        email: "",
+        role: "",
+    });
+}
