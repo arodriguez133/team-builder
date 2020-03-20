fbdaec04cf4fa5ccbddf3f2a7b3f8cb227485f5d
Adan Rodriguez <arodriguez_11@live.com>
2020-03-19 20:12:38 -0500

    finished Form file

--- src/Form.js
+++ src/Form.js
@@ -10,3 +10,39 @@ const Form = props => {
         role: "",
     });
 }
+
+//Keeps track of changes
+
+const eventHandler = event => {
+    console.log(event);
+    setCard({...card, [event.target.name]: event.target.value});
+};
+
+//Submits data and creates new card
+const submitForm = event => {
+    event.preventDefault();
+    //addMember is defined in app.js
+    props.addMember(card);
+};
+
+return(
+    <div>
+        <form onSubmit ={submitForm}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name"
+            onChange={eventHandler} placeholder = "Name" />
+            <label htmlFor ="email">Email</label>
+            <input type ="text" id="email" name="Email"
+            onChange ={eventHandler} placeholder = "Email"/>
+
+            <label htmlFor ="role">Role</label>
+            <input type = "text" id="role" name="role"
+            onChange=(eventHandler) placeholder="Role"/>
+
+            <button type = "submit">Submit</button>
+
+        </form>
+    </div>
+)
+
+export default Form;
